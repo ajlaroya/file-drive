d4a29dc0bafa88480e157e7b8a658964932d94f5
ajlaroya <arthur.j.laroya@gmail.com>
2024-05-02 12:41:55 +1000

    schema and auth in convex

--- convex/schema.ts
+++ convex/schema.ts
@@ -1,1 +1,6 @@
-//23:09
+import { defineSchema, defineTable } from "convex/server";
+import { v } from "convex/values";
+
+export default defineSchema({
+  files: defineTable({ name: v.string() }),
+});

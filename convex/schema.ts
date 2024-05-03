a1b2f1ba73986e14317d824e78ffdb3f658be38f
ajlaroya <arthur.j.laroya@gmail.com>
2024-05-03 15:15:52 +1000

    orgid scope files, clerk webhooks

--- convex/schema.ts
+++ convex/schema.ts
@@ -4,4 +4,8 @@ import { v } from "convex/values";
 export default defineSchema({
   files: defineTable({ name: v.string(), orgId: v.string() })
     .index("by_orgId", ["orgId"]),
+  users: defineTable({
+    tokenIdentifier: v.string(),
+    clerkId: v.string(),
+  })
 });

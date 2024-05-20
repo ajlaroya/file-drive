0b05437e04c805faddd9e5cc579edc032b1aa097
ajlaroya <arthur.j.laroya@gmail.com>
2024-05-20 14:54:38 +1000

    auth based on org id and tokenIdentifier

--- convex/files.ts
+++ convex/files.ts
@@ -1,5 +1,19 @@
 import { ConvexError, v } from "convex/values";
-import { mutation, query } from "./_generated/server";
+import { MutationCtx, QueryCtx, mutation, query } from "./_generated/server";
+import { getUser } from "./users";
+
+async function hasAccessToOrg(
+  ctx: QueryCtx | MutationCtx,
+  tokenIdentifier: string,
+  orgId: string
+) {
+  const user = await getUser(ctx, tokenIdentifier);
+
+  const hasAccess =
+    user.orgIds.includes(orgId) || user.tokenIdentifier.includes(orgId);
+
+  return hasAccess;
+}
 
 export const createFile = mutation({
   args: {
@@ -11,6 +25,16 @@ export const createFile = mutation({
 
     if (!identity) {
       throw new ConvexError("you must be logged in to upload a file");
+    }
+
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      args.orgId
+    );
+
+    if (!hasAccess) {
+      throw new ConvexError("you do not have access to this org");
     }
 
     await ctx.db.insert("files", {
@@ -28,6 +52,16 @@ export const getFiles = query({
     const identity = await ctx.auth.getUserIdentity();
 
     if (!identity) {
+      return [];
+    }
+
+    const hasAccess = await hasAccessToOrg(
+      ctx,
+      identity.tokenIdentifier,
+      args.orgId
+    );
+
+    if (!hasAccess) {
       return [];
     }
 
